be5e3ebf3a0523a24734f555430f68ebe38c17c6
Lorenz Hörburger <lorenz.hoerburger@gmx.at>
2024-05-06 11:26:11 +0200

    add navigation

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import NavHeader from "@/components/NavHeader.vue";
 </script>
 
 <template>
-  <header class="bg-gray-800 py-4 flex gap-4 items-center">
-    <h1 class="text-4xl font-semibold text-red-500">Hello World</h1>
-    <div>
-      <nav class="flex gap-2">
-        <RouterLink to="/" class="bg-white p-2 rounded">Dashboard</RouterLink>
-        <RouterLink to="/vehicles" class="bg-white p-2 rounded"
-          >Vehicles</RouterLink
-        >
-      </nav>
-    </div>
-  </header>
+  <div>
+    <header>
+      <NavHeader />
+    </header>
 
-  <RouterView />
+    <RouterView />
+  </div>
 </template>

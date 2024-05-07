b6fb30f01e9df9373a30f51c6d492134a7fd1370
Lorenz Hörburger <lorenz.hoerburger@gmx.at>
2024-05-07 10:16:05 +0200

    update log view title

--- web/src/views/LogView.vue
+++ web/src/views/LogView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="font-semibold text-3xl pb-2">Inventory</h1>
+    <h1 class="font-semibold text-3xl pb-2">Logs</h1>
     <div class="card" v-if="loading">
       <div class="animate-pulse w-100">
         <div v-for="idx in 20" :key="idx" class="h-4 bg-gray-200 rounded-full w-full mb-4"></div>

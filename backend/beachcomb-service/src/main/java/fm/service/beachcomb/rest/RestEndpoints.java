fd1fddea652e04f7f1a8fa4ed39c713439a5219c
LFQO <70878450+LFQO@users.noreply.github.com>
2024-06-06 18:02:28 +0200

    rest api docs

--- backend/beachcomb-service/src/main/java/fm/service/beachcomb/rest/RestEndpoints.java
+++ backend/beachcomb-service/src/main/java/fm/service/beachcomb/rest/RestEndpoints.java
@@ -3,7 +3,13 @@ package fm.service.beachcomb.rest;
 import fm.api.datafeeder.VehicleDataDTO;
 import fm.api.inventory.VehicleType;
 import fm.service.beachcomb.mongo.controller.MongoController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +22,34 @@ public class RestEndpoints {
     @Autowired
     MongoController controller;
 
+    @Operation(summary = "Find candidates for matching with the given vehicle")
+    @ApiResponse(responseCode = "200", description = "Found successfully",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = VehicleDataDTO.class)))})
+    @ApiResponse(responseCode = "400", description = "Invalid vin supplied", content = @Content)
+    @ApiResponse(responseCode = "404", description = "Candidates not found", content = @Content)
     @GetMapping(value = "/beachcomb/{vin}")
     @ResponseBody
     public List<VehicleDataDTO> matching(@PathVariable("vin") String vin, @RequestParam("type") VehicleType type) {
         return nearest.findMatches(vin, type);
     }
 
+    @Operation(summary = "Get data of the given vehicle")
+    @ApiResponse(responseCode = "200", description = "Found successfully",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = VehicleDataDTO.class))})
+    @ApiResponse(responseCode = "400", description = "Invalid vin supplied", content = @Content)
+    @ApiResponse(responseCode = "404", description = "Data not found", content = @Content)
     @GetMapping(value = "/beachcomb/vehicles/{vin}")
     public VehicleDataDTO getVehicle(@PathVariable("vin") String vin) {
         return controller.findVehicleByVin(vin);
     }
 
+    @Operation(summary = "Get data of all available vehicles")
+    @ApiResponse(responseCode = "200", description = "Found successfully",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = VehicleDataDTO.class)))})
+    @ApiResponse(responseCode = "404", description = "Data not found", content = @Content)
     @GetMapping(value = "/beachcomb/vehicles")
     public List<VehicleDataDTO> getAll() {
         return controller.getVehicles();
--- backend/control-service/src/main/java/fm/service/control/rest/RestEndpoints.java
+++ backend/control-service/src/main/java/fm/service/control/rest/RestEndpoints.java
@@ -1,8 +1,15 @@
 package fm.service.control.rest;
 
+import fm.api.datafeeder.VehicleDataDTO;
 import fm.api.datafeeder.VehicleStatusDTO;
 import fm.service.control.mongo.controller.MongoController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,12 +21,23 @@ public class RestEndpoints {
     @Autowired
     MongoController controller;
 
+    @Operation(summary = "Get status of the given vehicle")
+    @ApiResponse(responseCode = "200", description = "Found successfully",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = VehicleStatusDTO.class))})
+    @ApiResponse(responseCode = "400", description = "Invalid vin supplied", content = @Content)
+    @ApiResponse(responseCode = "404", description = "Status not found", content = @Content)
     @GetMapping(value = "/control/status/{vin}")
     @ResponseBody
     public VehicleStatusDTO getStatus(@PathVariable("vin") String vin) {
         return controller.findByVin(vin);
     }
 
+    @Operation(summary = "Get status of all available vehicles")
+    @ApiResponse(responseCode = "200", description = "Found successfully",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = VehicleStatusDTO.class)))})
+    @ApiResponse(responseCode = "404", description = "Status not found", content = @Content)
     @GetMapping(value = "/control/status")
     @ResponseBody
     public List<VehicleStatusDTO> getAll() {

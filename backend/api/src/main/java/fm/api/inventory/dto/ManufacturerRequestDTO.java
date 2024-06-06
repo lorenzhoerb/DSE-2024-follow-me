8d16c4a921ad68610b722634835a8d126c564af9
Lorenz Hörburger <lorenz.hoerburger@gmx.at>
2024-06-06 21:37:43 +0200

    add api docs for inventory service

--- backend/api/src/main/java/fm/api/inventory/dto/ManufacturerRequestDTO.java
+++ backend/api/src/main/java/fm/api/inventory/dto/ManufacturerRequestDTO.java
@@ -1,5 +1,6 @@
 package fm.api.inventory.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -13,8 +14,16 @@ import lombok.NoArgsConstructor;
 public class ManufacturerRequestDTO {
     @NotNull(message = "name is required")
     @NotBlank(message = "name is required")
+    @Schema(
+            description = "Name of manufacturer",
+            example = "Mercedes"
+    )
     private String name;
     @NotNull(message = "manufacturerCode Is required")
     @Size(min = 3, max = 5, message = "manufacturerCode must be between 3 and 5 characters")
+    @Schema(
+            description = "Unique manufacturer coded",
+            example = "MEC"
+    )
     private String manufacturerCode;
 }
--- backend/inventory-service/src/main/java/fm/service/inventory/controller/ManufacturerController.java
+++ backend/inventory-service/src/main/java/fm/service/inventory/controller/ManufacturerController.java
@@ -3,7 +3,14 @@ package fm.service.inventory.controller;
 import fm.api.inventory.dto.ManufacturerDetailsDTO;
 import fm.api.inventory.dto.ManufacturerRequestDTO;
 import fm.service.inventory.service.IManufacturerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,11 +25,22 @@ public class ManufacturerController {
         this.manufacturerService = manufacturerService;
     }
 
+    @Operation(summary = "Get all manufacturers", description = "Returns a list of all manufacturers")
+    @ApiResponse(responseCode = "200", description = "Found successfully",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = ManufacturerDetailsDTO.class)))})
     @GetMapping
     public List<ManufacturerDetailsDTO> findAll() {
         return manufacturerService.findAll();
     }
 
+    @Operation(summary = "Create a new manufacturer", description = "Creates a new manufacturer and returns the created manufacturer details")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Manufacturer created"),
+                    @ApiResponse(responseCode = "400", description = "Validation error"),
+                    @ApiResponse(responseCode = "409", description = "Conflict: Manufacturer with manufacturer code already exists"),
+            })
     @PostMapping
     public ManufacturerDetailsDTO createManufacturer(@Valid @RequestBody ManufacturerRequestDTO manufacturer) {
         return manufacturerService.create(manufacturer);

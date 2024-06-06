919082975eb94180f8b4ee6fc7a075e29cffae2e
Lorenz Hörburger <lorenz.hoerburger@gmx.at>
2024-06-06 21:41:45 +0200

    add api docs for event service

--- backend/event-service/src/main/java/fm/service/event/EventController.java
+++ backend/event-service/src/main/java/fm/service/event/EventController.java
@@ -2,6 +2,9 @@ package fm.service.event;
 
 import fm.api.common.EventMessageDTO;
 import fm.api.common.LogLevel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +19,12 @@ public class EventController {
         this.eventService = eventService;
     }
 
+    @Operation(summary = "Get all events", description = "Retrieves a list of all events, optionally filtered by log level")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Events retrieved successfully"),
+                    @ApiResponse(responseCode = "400", description = "Invalid log level")
+            })
     @GetMapping
     public List<EventMessageDTO> getAllEvents(@RequestParam("level") Optional<LogLevel> level) {
         return eventService.findAll(level);
